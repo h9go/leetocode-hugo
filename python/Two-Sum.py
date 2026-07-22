7a7eeddb4b59a9834c03d4ec46bec80520fd5170
hugo <109176014+h9go@users.noreply.github.com>
2026-07-22 18:02:09 +0200

    Add files via upload

---
+++ python/Two-Sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(0, len(nums)):
+            n1 = nums[i]
+            for j in range(i+1, len(nums)):
+                n2 = nums[j]
+                if n1 + n2 == target:
+                    ris = [i, j]
+                    return ris
